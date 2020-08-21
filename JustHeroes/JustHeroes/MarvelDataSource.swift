560e34cbe18d9f24b714852231e53c05f8ef3662
Daniel Méndez <daniel.mendez@edreamsodigeo.com>
2020-08-21 02:07:35 +0200

    added repository datasourse and api to do the request for characters

--- JustHeroes/JustHeroes/MarvelDataSource.swift
+++ JustHeroes/JustHeroes/MarvelDataSource.swift
@@ -1,22 +1,65 @@
 import Foundation
+import CryptoSwift
 
 struct MarvelCharacterDTO: Codable {}
 
-class MarvelCharacterDataSource: CharactersRepositoryDataSource {
+class MarvelDataSource: CharactersRepositoryDataSource {
     
     typealias DTO = MarvelCharacterDTO
     
-    static var apiKey: String? {
-        ProcessInfo.processInfo.environment["MARVEL_API_KEY"]
+    static var apiKey: String {
+        guard let apiKey = ProcessInfo.processInfo.environment["MARVEL_API_KEY"] else {
+            fatalError("Missing api key at the enviroment")
+        }
+        return apiKey
     }
+    
+    static private var privateKey: String {
+        guard let apiKey = ProcessInfo.processInfo.environment["MARVEL_PRIVATE_KEY"] else {
+            fatalError("Missing api key at the enviroment")
+        }
+        return apiKey
+    }
+    
+    static let baseURL = "https://gateway.marvel.com:443/v1/public/"
     
     enum Operation {
-        case <#case#>
+        case character(limit: Int, offset: Int)
+        
+        var properties: (path: String, method: HTTPMethod, parameters: Parameters) {
+            switch self {
+            case .character(let limit, let offset):
+                let now = Date().timeIntervalSince1970
+                
+                let parameters: Parameters = [
+                    "apikey": MarvelDataSource.apiKey,
+                    "limit": limit,
+                    "offset": offset,
+                    "ts": now,
+                    "hash": "\(now)\(MarvelDataSource.privateKey)\(MarvelDataSource.apiKey)".md5()
+                ]
+                return (path: "characters", method: .GET, parameters: parameters)
+            }
+        }
     }
-    let urlString = "https://gateway.marvel.com:443/v1/public/characters?apikey=885d2218c82dce59c8159ef7d42823fe"
+    
+    let api: BaseAPI
+    
+    init(api: BaseAPI) {
+        self.api = api
+    }
     
     func fetchCharacters(withLimit limit: Int, offset: Int, callback: @escaping (Result<MarvelCharacterDTO, Error>) -> Void) {
-        
+        let properties = Operation.character(limit: limit, offset: offset).properties
+        do {
+            try api.request(forPath: properties.path,
+                        method: properties.method,
+                        withParameters: properties.parameters) { (result: Result<MarvelCharacterDTO, Error>) in
+                callback(result)
+            }
+        } catch {
+            callback(.failure(error))
+        }
     }
     
 }
@@ -30,24 +73,28 @@ enum HTTPMethod: String {
 enum APIError: Error {
     case invalidURL
     case invalidComponents
+    case noData
+    case unableToParse
 }
 
 class BaseAPI {
     
     let baseURL: URL
+    let session: URLSession
     
-    init(baseURL: String) throws {
+    init(baseURL: String, session: URLSession) throws {
         guard let url = URL(string: baseURL) else {
             throw APIError.invalidURL
         }
         self.baseURL = url
+        self.session = session
     }
     
     var defaultHeaders: [String: String] {
         [:]
     }
     
-    func makeRequest(forPath path: String,
+    private func makeRequest(forPath path: String,
                      method: HTTPMethod,
                      withParameters parameters: Parameters) throws -> URLRequest {
         let url = baseURL.appendingPathComponent(path)
@@ -72,5 +119,56 @@ class BaseAPI {
         request.allHTTPHeaderFields = defaultHeaders
         
         return request
+    }
+    
+    func request<T: Decodable>(forPath path: String,
+                 method: HTTPMethod,
+                 withParameters parameters: Parameters,
+                 callback: @escaping (Result<T, Error>) -> Void) throws {
+        let request = try makeRequest(forPath: path, method: method, withParameters: parameters)
+        let task = session.dataTask(with: request) { [weak self] (data, response, error) in
+            
+            if let error = error {
+                return callback(.failure(error))
+            }
+            
+            self?.processData(data: data, callback: callback)
+            
+            if let response = response, let data = data {
+                URLCache.shared.storeCachedResponse(
+                    CachedURLResponse(
+                        response: response,
+                        data: data),
+                    for: request
+                )
+            }
+        }
+        URLCache.shared.getCachedResponse(for: task) { [weak self] (response) in
+            guard let response = response else {
+                return task.resume()
+            }
+            self?.processData(data: response.data, callback: callback)
+        }
+    }
+    
+    private func processData<T: Decodable>(data: Data?, callback: @escaping (Result<T, Error>) -> Void) {
+        do {
+            guard let data = data else {
+                throw APIError.noData
+            }
+            let jsonObject: T = try JSONSerialization.jsonObject(with: data)
+            return callback(.success(jsonObject))
+        } catch {
+            callback(.failure(error))
+        }
+    }
+}
+
+extension JSONSerialization {
+    static func jsonObject<T>(with data: Data) throws -> T {
+        guard let object = try JSONSerialization.jsonObject(with: data, options: []) as? T else {
+            throw APIError.unableToParse
+        }
+        return object
     }
 }
--- JustHeroes/JustHeroesTests/CharactersRepositoryTest.swift
+++ JustHeroes/JustHeroesTests/CharactersRepositoryTest.swift
@@ -7,27 +7,34 @@
 //
 
 import XCTest
+@testable import JustHeroes
+
+struct CharactersRepositoryMapperMock: CharactersRepositoryMapper {
+    func map<T>(fromObject object: T) -> [JHCharacter] where T : Decodable, T : Encodable {
+        return []
+    }
+}
 
 class CharactersRepositoryTest: XCTestCase {
 
+    var sut: CharactersRepository<MarvelDataSource>!
+    
     override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        let api = try! BaseAPI(baseURL: MarvelDataSource.baseURL, session: .init(configuration: .default))
+        let dataSource = MarvelDataSource(api: api)
+        sut = CharactersRepository(pageSize: 20, dataSource: dataSource, mapper: CharactersRepositoryMapperMock())
     }
 
     func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
+        let expect = expectation(description: "api call")
 
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
         self.measure {
-            // Put the code you want to measure the time of here.
+            sut.fetchCharacters(atPage: 1) { (result) in
+                expect.fulfill()
+            }
         }
+
+        wait(for: [expect], timeout: .pi)
     }
 
 }
