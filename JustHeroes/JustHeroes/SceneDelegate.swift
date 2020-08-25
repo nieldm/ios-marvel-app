397f569f706fc71b2453a9f5cd8ea4017443d5ba
Daniel Méndez <daniel.mendez@edreamsodigeo.com>
2020-08-25 15:34:27 +0200

    moved process info to a separated file

--- JustHeroes/JustHeroes/SceneDelegate.swift
+++ JustHeroes/JustHeroes/SceneDelegate.swift
@@ -1,44 +1,5 @@
-//
-//  SceneDelegate.swift
-//  JustHeroes
-//
-//  Created by Daniel Mendez on 8/18/20.
-//  Copyright © 2020 Daniel Mendez. All rights reserved.
-//
-
 import UIKit
 import SwiftUI
-
-enum EnviromentOption: String {
-    case startView = "START_VIEW"
-    case marvelAPIkey = "MARVEL_API_KEY"
-    case marvelAPIprivateKey = "MARVEL_PRIVATE_KEY"
-    case mockServer = "MOCK_SERVER"
-}
-
-extension ProcessInfo {
-    
-    func getEnviromentOption(_ option: EnviromentOption) -> String? {
-        return environment[option.rawValue]
-    }
-    
-    func getStartView() -> StartView {
-        let startView = getEnviromentOption(.startView) ?? ""
-        return StartView(rawValue: startView) ?? StartView.none
-    }
-    
-    func mockServer() -> Bool {
-        getEnviromentOption(.mockServer) == "YES"
-    }
-}
-
-enum StartView: String {
-    case test
-    case characterList
-    case sortFilter
-    case comics
-    case none = ""
-}
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
